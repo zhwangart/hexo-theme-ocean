602e684d7155942cdaa22568536591cf61ffaa22
wangzh <wangzh@appexnetworks.com>
2018-11-19 18:52:47 +0800

    精简

--- source/js/script.js
+++ source/js/script.js
@@ -30,6 +30,33 @@
             $searchWrap.removeClass('on');
         }
     });
+    // 移动设备侦测
+    var isMobile = {
+        Android: function () {
+            return navigator.userAgent.match(/Android/i);
+        },
+        BlackBerry: function () {
+            return navigator.userAgent.match(/BlackBerry/i);
+        },
+        iOS: function () {
+            return navigator.userAgent.match(/iPhone|iPad|iPod/i);
+        },
+        Opera: function () {
+            return navigator.userAgent.match(/Opera Mini/i);
+        },
+        Windows: function () {
+            return navigator.userAgent.match(/IEMobile/i);
+        },
+        any: function () {
+            return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows());
+        }
+    };
+    // 建议在移动端不初始化，其实 /search.xml 文件还挺大的，
+    if ($('.local-search').size() && !isMobile.any()) {
+        $.getScript('/js/search.js', function() {
+            searchFunc("/search.xml", 'local-search-input', 'local-search-result');
+        });
+    }
 
     // Share ------------
     $('body').on('click', function () {
